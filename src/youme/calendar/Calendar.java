8fede06abc58c6a585f80b906e24b29a286e44f7
youme <youme@DESKTOP-7HTC1AP>
2017-07-20 01:55:08 +0900

    update using class method

--- src/youme/calendar/Calendar.java
+++ src/youme/calendar/Calendar.java
@@ -3,7 +3,14 @@ package youme.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	public static void main(String args[]) {
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonths(int month) { // int로 입력받아서 int로 되돌려준다는 메소드
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalendar() { //입력도 출력도 없는 메소드
 		System.out.println("Hello, Calendar");
 		System.out.println("일   월    화   수  목   금   토");
 		System.out.println("--------------------");
@@ -11,20 +18,19 @@ public class Calendar {
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
-		
-		//숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
+	}
+
+	public static void main(String args[]) {
+
+		// 숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
 		Scanner scan = new Scanner(System.in);
-	    System.out.println("원하는 달을 입력해주세요. 예)1월~12월");
-		int month=scan.nextInt();
-		System.out.println(month+"월");
-		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
-		System.out.printf("%d월은 %d일 까지 있습니다.\n",month,maxDays[month-1]);
+		Calendar cal = new Calendar();
+		System.out.println("원하는 달을 입력해주세요. 예)1월~12월");
+		int month = scan.nextInt();
+		System.out.println(month + "월");
+
+		System.out.printf("%d월은 %d일 까지 있습니다.\n", month, cal.getMaxDaysOfMonths(month));
 		scan.close();
-		
-		
-		
+
 	}
 }
