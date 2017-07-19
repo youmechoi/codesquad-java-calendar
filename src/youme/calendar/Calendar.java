9e5888d1f402ca37dd0ea085b53d27ebd9256a65
youme <youme@DESKTOP-7HTC1AP>
2017-07-20 02:36:32 +0900

    update while, for

--- src/youme/calendar/Calendar.java
+++ src/youme/calendar/Calendar.java
@@ -37,19 +37,31 @@ public class Calendar {
 	public static void main(String args[]) {
 
 		// 숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
+		String PROMPT = "cal> ";
 		Scanner scan = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		
-		System.out.println("원하는 반복 횟수를 입력하세요");
-		int repeat = scan.nextInt();
-		//횟수를 모를때는 while문, 횟수를 잘 알떄는 for문을 사용하는게 좋다
-		
-		for(int i=0; i<repeat; i++){ //이거는 그냥 repeat번 반복하는 반복문 for문이다		
-		System.out.println("원하는 달을 입력해주세요. 예)1월~12월");
-		int month = scan.nextInt();
-		System.out.printf("%d월은 %d일 까지 있습니다.\n", month, cal.getMaxDaysOfMonths(month));
-		}		
-		System.out.println("bye~*");	
+
+		// System.out.println("원하는 반복 횟수를 입력하세요");
+
+		// int repeat;
+		// 횟수를 모를때는 while문, 횟수를 잘 알떄는 for문을 사용하는게 좋다
+
+		// for(int i=0; i<repeat; i++){ //이거는 그냥 repeat번 반복하는 반복문 for문이다
+
+		while (true) {
+			System.out.println("원하는 달을 입력해주세요. 예)1월~12월");
+			System.out.print(PROMPT);
+			int month = scan.nextInt();
+			if(month == -1){
+				break;
+			}
+			if(month > 12){
+				continue; //continue를 만나면 루프의 처음으로 돌아간다
+			} 
+			System.out.printf("%d월은 %d일 까지 있습니다.\n", month, cal.getMaxDaysOfMonths(month));
 		}
-	
+		System.out.println("bye~*");
+		scan.close();
+	}
+
 }
